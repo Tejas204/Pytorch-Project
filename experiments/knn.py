d35e8f2cd9d1cc3b06e057abf28e7309f2bc586b
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-09-21 12:05:32 +0200

    KNN Algorithm

--- experiments/knn.py
+++ experiments/knn.py
@@ -2,7 +2,8 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-import seaborn as sns
+import random
+from collections import Counter
 
 # ********************************************************************************************
 # TRAIN DATA POINTS
@@ -11,14 +12,12 @@ import seaborn as sns
 # Class variables: 0 --> A, 1 --> B, 2 --> C
 # ********************************************************************************************
 
-train_class_variable = np.random.randint(3, size=300)
+train_class_variable = np.random.randint(3, size=30)
 print(f"--> Class variables:\n{train_class_variable}\n")
 
 
-train_data_variable = np.random.random_sample((300, ))
+train_data_variable = np.random.random_sample((30, ))
 print(f"--> Data points:\n{train_data_variable.size}\n")
-# plt.plot(train_data_variable, 'bo')
-# plt.show()
 
 train_data_dictionary = {key:value for key, value in zip(train_data_variable, train_class_variable)}
 
@@ -40,13 +39,45 @@ print(f"--> Test data set:\n{test_data_dictionary}\n")
 fig, ax = plt.subplots()
 for data_class in [0, 1, 2]:
     if data_class == 0:
-        color = 'r+'
+        color = 'ro'
     elif data_class == 1:
         color = 'bo'
     else:
-        color = 'g*'
+        color = 'go'
 
     plt.plot({key: value for key, value in train_data_dictionary.items() if value == data_class}.keys(), color, label="Class "+str(data_class))
     ax.legend()
 
 plt.show()
+
+# ********************************************************************************************
+# ALGORITHM
+
+# ********************************************************************************************
+
+k = 5
+
+for test_data_point in test_data_variable:
+    # Calculate distance to each point
+    distance = []
+    for train_data_point in list(train_data_dictionary.keys()):
+        distance.append(np.square(test_data_point - train_data_point))
+
+    # Find top k points and their classes
+    distance.sort()
+    top_k = distance[:k]
+    top_k_dictionary = {key:value for key, value in train_data_dictionary.items() if key in top_k}
+    print(top_k_dictionary)
+
+    # Calculate majority vote
+    votes = Counter(top_k_dictionary.values())
+    classes = list(votes.keys())
+    counts = list(votes.values())
+    class_allotment = classes.index(counts.index(max(counts)))
+
+    # Update dictionary
+    test_data_dictionary.update({test_data_point:class_allotment})
+
+
+
+
