3c7a82b1ab5bf9e098e614ab8d31bb469d6066f5
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-11-02 22:26:45 +0100

    CNN Image Classification

--- tutorial/13_CNN.py
+++ tutorial/13_CNN.py
@@ -26,3 +26,69 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bat
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+class ConvNet(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+model = ConvNet().to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# Train loop
+print(f"\n Training Begins!")
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+
+        # Loss
+        loss = criterion(outputs, labels)
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 2000 == 0:
+            print(f"Epoch: {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}")
+
+print(f"\n Finished Training!")
+
+# Testing
+# Testing
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+        # Value and index
+        _, predictions = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predictions == labels).sum().item()
+
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predictions[i]
+            if label == pred:
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+    
+    accuracy = 100.0 * n_correct / n_samples
+    print(f"Test accuracy: {accuracy}")
